33387f7d08c50192a7c0cbaac00b2057bcc9869b
omarrr854 <omarruelasglz23@gmail.com>
2026-06-25 15:48:04 -0600

    Ajustes dimensiones

--- script.js
+++ script.js
@@ -57,9 +57,11 @@ const productos = [
   id: 6,
      nombre: 'Playera estampada "Endless summer"',
     precio: 199,
-     descripcion: 'Playera color rojo con un diseño en la parte trasera tipo playa, un gran diseño para el verano o para lucir por la calle un dia casual. En la parte de enfrente debajo del cuello la frase "Endless summer" que significa: "Un verano sin fin".',
+     descripcion: 'Playera con diseño en la parte trasera tipo playa, un gran diseño para el verano o para lucir por la calle un dia casual. En la parte de enfrente debajo del cuello la frase "Endless summer" que significa: "Un verano sin fin".',
     variantes: [
-      { color: 'Rojo', imagen: 'EndlessSummer.png' }
+      { color: 'Rojo', imagen: 'EndlessSummer.png' },
+      { color: 'Verde Oscuro', imagen: 'EndlessSummerVerde.png' },
+      { color: 'Negro', imagen: 'EndlessSummerNegro.png' }
     ]
   }
 ];
